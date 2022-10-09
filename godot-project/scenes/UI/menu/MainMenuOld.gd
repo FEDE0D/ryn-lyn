bf1ab1e0065121aaa4156d4444004faee4051c7d
Federico Pacheco <federicogpacheco@gmail.com>
2022-10-09 19:39:56 -0300

    update main menu

--- godot-project/scenes/UI/menu/MainMenuOld.gd
+++ godot-project/scenes/UI/menu/MainMenuOld.gd
@@ -1,14 +1,14 @@
 extends Control
 
 func _ready():
-	$"%PlayBtn".grab_focus()
+	$"%PlayButton".grab_focus()
 
-func _on_PlayBtn_pressed():
+func _on_PlayButton_pressed():
 	GameState.start_game()
 	get_tree().change_scene("res://scenes/Game/World1.tscn")
 
-func _on_OptionsBtn_pressed():
+func _on_OptionsButton_pressed():
 	get_tree().change_scene("res://scenes/UI/menu/OptionsMenu.tscn")
 
-func _on_QuitBtn_pressed():
+func _on_SalirButton_pressed():
 	get_tree().quit()
